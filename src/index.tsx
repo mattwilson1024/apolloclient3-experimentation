562f881af7a3db14aa2079053438a97c535d63ee
Matt Wilson <matt@matt-wilson.org>
2020-02-19 00:59:09 +0000

    Show toast when network request happens

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,30 @@
 import './index.css';
+import 'react-toastify/dist/ReactToastify.css';
 
-import { ApolloClient, ApolloProvider, HttpLink, InMemoryCache } from '@apollo/client';
+import { ApolloClient, ApolloLink, ApolloProvider, HttpLink, InMemoryCache } from '@apollo/client';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { toast } from 'react-toastify';
 
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+toast.configure();
+
+// Custom Apollo link which pops up a toast notification whenever network requests are happening, just to make the demo clearer
+const networkActivityToastLink = new ApolloLink((operation, forward) => {
+  toast("Network request!");
+  return forward(operation);
+});
+
+// HttpLink which connects to an example public GraphQL API for Spotify, see https://github.com/lowsky/spotify-graphql-server
+const httpLink = new HttpLink({
+  uri: 'https://spotify-graphql-server.herokuapp.com/graphql',
+});
+
 const client = new ApolloClient({
-  cache: new InMemoryCache({
-    
-  }),
-  link: new HttpLink({
-    uri: 'https://spotify-graphql-server.herokuapp.com/graphql',
-  })
+  cache: new InMemoryCache({}),
+  link: networkActivityToastLink.concat(httpLink)
 });
 
 ReactDOM.render(
